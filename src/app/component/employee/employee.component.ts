4628d920d30bf9d61e45a89de76baa81d3b2e7ec
ahadAli102 <ahad15-12484@diu.edu.bd>
2023-02-03 11:05:24 +0600

    get and delete

--- src/app/component/employee/employee.component.ts
+++ src/app/component/employee/employee.component.ts
@@ -1,5 +1,7 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Employee } from 'src/app/model/employee';
 import { EmployeeService } from 'src/app/service/employee.service';
 
 @Component({
@@ -9,22 +11,50 @@ import { EmployeeService } from 'src/app/service/employee.service';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor(private employeeService: EmployeeService,private router: Router) {
+  allEmployees: Employee[] = [];
+  showEmployee = false;
+
+  constructor(private employeeService: EmployeeService, private router: Router) {
 
   } ngOnInit(): void {
+    const ref = this;
     this.employeeService.getAllEmployees().subscribe({
-      next(response: any) {
-        console.log('Employee ',response);
+      next(response: Employee[]) {
+        ref.showEmployee = response.length > 0;
+        ref.allEmployees = response;
       }, error(message: any) {
-        console.log('error',message);
+        console.log('error', message);
       }, complete() {
         console.log('complete');
       }
     });
+
+    this.employeeService
   }
 
   update(id: number) {
     this.router.navigateByUrl(`/employee/edit/${id}`);
+  }
+
+  delete(id: number) {
+    const ref = this;
+    console.log(`delete ${id}`);
+    this.employeeService.deleteEmployee(id).subscribe({
+      next(response) {
+        let index = -1;
+        ref.allEmployees.forEach((employee, i) => {
+          if (employee.id === id) {
+            index = i;
+          }
+        })
+        ref.allEmployees.splice(index, 1);
+        ref.showEmployee = ref.allEmployees.length > 0;
+      }, error(message: any) {
+        console.log('error', message);
+      }, complete() {
+        console.log('complete');
+      }
+    })
   }
 }
 
--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,17 +1,33 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { Employee } from '../model/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
   
+  private API_URL = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
 
-  getAllEmployees() {
-    return this.http.get<any>('http://localhost:8080/hsenid/api/v1/employees');
+  getAllEmployees():Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.API_URL+'/employees').pipe(
+      map(employees=> employees.map(employee => ({
+        id:employee.id,
+        firstName:employee.firstName,
+        lastName:employee.lastName,
+        designation:employee.designation,
+        email:employee.email === null ? 'No email' : employee.email,
+      })))
+    );
+  }
+
+  deleteEmployee(id: number) {
+    return this.http.delete(this.API_URL + '/employees/' + id);
   }
 
 
